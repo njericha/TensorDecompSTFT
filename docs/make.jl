6c05ea44a768f6d356c7bce1e817fc19902d15dc
Nicholas Richardson <njericha@uwaterloo.ca>
2022-11-05 21:52:56 -0700

    Create docs

---
+++ docs/make.jl
@@ -0,0 +1,4 @@
+push!(LOAD_PATH, "..\\src\\")
+using Documenter, TensorDecompSTFT
+
+makedocs(sitename="Musical STFT Tensor Decomposition")
--- src/TensorDecompSTFT.jl
+++ src/TensorDecompSTFT.jl
@@ -1,5 +1,12 @@
 module TensorDecompSTFT
 
+export greet
+
+"""
+    greet(::Any)
+
+Prints "Hello World!"
+"""
 greet() = print("Hello World!")
 
 end # module TensorDecompSTFT
