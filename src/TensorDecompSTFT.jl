15148c7d929587bda06840b631a71cf5f1d3becd
Nicholas Richardson <njericha@uwaterloo.ca>
2022-11-15 15:00:49 -0800

    Remove mySTFT and add useful wrappers

--- src/TensorDecompSTFT.jl
+++ src/TensorDecompSTFT.jl
@@ -1,6 +1,6 @@
 module TensorDecompSTFT
 
-export greet, mySTFT
+export greet
 
 """
     greet(::Any)
@@ -9,66 +9,41 @@ Prints "Hello World!"
 """
 greet() = print("Hello World!")
 
-using FFTW
+using STFT
 
 """
-    _stft(x_odd_extention, window)
-Helper function that takes the odd extention of x (length 3N
-where N isthe number of samples of x), and a window of length 2N,
-abd computes the fft of the windowed signal.
+    hann(n::Int)
+Makes a Hann window with N samples with peak value of 1.
+If N is even, will return N+1 samples.
 """
 
-function _stft(x_odd_extention, window)
-    N = length(window)÷2 #original length of signal
-    n_time_steps = N
-    X = zeros(Complex, N, n_time_steps)
-    for i ∈ 1:n_time_steps
-        left  = i
-        right = i + 2N-1
-        slice = left:right
-        x_trunc = x_odd_extention[slice]
-        X[:,i]  = fft(x_trunc .* window)[1:N]#Just want the non negative frequencies
-    end
-    return X
+function hann(N::Int)
+    N = N - N % 2 #makes N even
+    n = 0:N
+    return @. sin(π*n/N)^2
 end
 
 """
-    mySTFT(x::Vector{Complex}, t::Vector{Real}, width::Real)
-
-Takes \$x(t)\$ and returns the STFT of x, X, and the frequencies k.
-A Gaussian window is used with the width needed as an input.
-
-Example:
-```julia
-t = Vector(range(0, 1, 1000)) # Time points
-x = sin.(2π*200t.^2) # Generate chirp signal
-width = 0.01 #window size
-X, k = mySTFT(x, t, width)
-s = abs2.(X) # get magnitude
-heatmap(t, k, s) # plot
-```
+    triange(n::Int)
+Makes a single triangle bump with n samples with peak value of 1
 """
-function mySTFT(x, t, width)#::Vector{Complex}, t::AbstractVector, width::Real)
-    # Make window
-    N = length(t)
-    right = (t[end] - t[1])
-    left  = - right #+ length(t) % 2
-    times = Vector(range(left, right, 2N))
-    window = exp.(-(times ./ width).^2 ./ 2)
-    
-    # Make odd extention of x
-    xr = -reverse(x)
-    odd_extention = cat(xr, x, xr, dims=1) # could make this better by not duplicating x(t[1])
-    
-    # Take STFT
-    X = _stft(odd_extention, window)#stft(odd_extention, window, N)#
-    
-    # Calculate frequencies
-    Δt = t[2] - t[1]
-    sample_rate = 1 / Δt
-    k = Vector(range(0, sample_rate/2, N))
 
-    return X, k
+function triangle(n::Int)
+    up   = range(0,1,length=(n+1)÷2)
+    down = reverse(up)[2:end]
+    return cat(up, down,dims=1)
+end
+
+"""
+    songread(n::Int)
+Wrapper to open a song file
+"""
+
+function songread(filename::String)
+    y, fs = wavread(filename)
+    N = length(y)
+    t = range(0, N/fs, length=N)
+    return y, t, fs
 end
 
 end # module TensorDecompSTFT
